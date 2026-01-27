927f06fca50a7cb37411c2fd4839f9fc9453e8b8
Sugoi <jgdgj223@gmail.com>
2026-01-27 21:42:47 +0900

    Remove app data directory

--- app/data/real/delta1-source.ts
+++
@@ -1,8 +0,0 @@
-import { openDelta1BasisSocket } from "../../api/delta1"
-import type { Delta1FeedSource } from "../delta1-source"
-
-export const realDelta1FeedSource: Delta1FeedSource = {
-  isMock: false,
-  openSocket: () => openDelta1BasisSocket(),
-  getSymbolSeeds: () => [],
-}
